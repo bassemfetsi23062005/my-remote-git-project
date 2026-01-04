5e16fd5b632a427764c7e52dd9a960e7a0f1cde1
bassem <bassemfetsi2005@gmail.com>
2026-01-04 15:54:44 +0100

    Initial commit: Setup Maven project structure.

---
+++ src/main/java/com/l3/gl/myRemoteGitProject/App.java
@@ -0,0 +1,10 @@
+package com.l3.gl.myRemoteGitProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
